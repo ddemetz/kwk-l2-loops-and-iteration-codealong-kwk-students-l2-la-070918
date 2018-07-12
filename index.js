261b29b7625a8719cb147636833ceb3ad0966f27
Dominique Demetz <ddemetz123@icloud.com>
2018-07-12 22:41:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const names = ['Ada','Brendan','Ali'];
+// const names = ['Ada','Brendan','Ali'];
 
-function printBadges(names){
+// function printBadges(names){
 
-for (let i = 0; i < names.length; i++){
-  names[i] = `Welcome ${names[i]}! You are employee #${i+1}.`;
-  console.log (names[i]);
+// for (let i = 0; i < names.length; i++){
+//   names[i] = `Welcome ${names[i]}! You are employee #${i+1}.`;
+//   console.log (names[i]);
 
 
-}
-return names
-}
+// }
+// return names
+// }
 
 function tailsNeverFails(){
   return Math.random() >=0.5;
